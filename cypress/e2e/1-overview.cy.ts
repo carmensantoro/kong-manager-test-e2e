c73857cd08db701722fe6263ccfd8272baae0288
Carmen <s.carmen2101@gmail.com>
2025-12-05 00:32:23 +0100

    create a test to check the initial count of the overview

---
+++ cypress/e2e/1-overview.cy.ts
@@ -0,0 +1,22 @@
+import { SummaryTypes } from "../support/types";
+import { Overview } from "../fixtures/1-overview";
+import { General } from "../fixtures/0-general";
+
+describe(`Kong Manager UI Test on desktop`, (): void => {
+  beforeEach(() => {
+    cy.cleanUpSummary(SummaryTypes.ALL);
+    cy.visit(`/default/overview`);
+  });
+
+  it("Check service and route count are 0 by default in Overview page", () => {
+    // It checks the loading screen is visible at first
+    cy.get(General.LOADING_PAGE).should("be.visible");
+    cy.get(General.LOADING_PAGE).should("not.exist");
+
+    cy.get(Overview.OVERVIEW_PAGE_TITLE).should("have.text", "Overview");
+    cy.get(Overview.OVERVIEW_SERVICES_COUNT).should("have.text", "0");
+    cy.get(Overview.OVERVIEW_ROUTES_COUNT).should("have.text", "0");
+    cy.get(Overview.OVERVIEW_CONSUMERS_COUNT).should("have.text", "0");
+    cy.get(Overview.OVERVIEW_PLUGINS_COUNT).should("have.text", "0");
+  });
+});

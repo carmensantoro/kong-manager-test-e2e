7c10c4bdb8a705a7158a176e856521c889728aba
Carmen <s.carmen2101@gmail.com>
2025-12-06 19:59:07 +0100

    add test result reporting and add a new configuration for the test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-      baseUrl: "http://localhost:8002",
-      env: {
-        ADMIN_API_URL: "http://localhost:8001"
-        },
-      // TODO: "Organize desktop, tablet and mobile tests. I set the viewport to 1280x720 for now."
-      viewportWidth: 1280,
-      viewportHeight: 720,
-        setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:8002",
+    env: {
+      ADMIN_API_URL: "http://localhost:8001",
+    },
+    // TODO: "Organize desktop, tablet and mobile tests. I set the viewport to 1280x720 for now."
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "cypress-report-config.json",
     },
   },
 });

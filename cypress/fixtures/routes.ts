db599aaaf412bc777dc121ba722feb9fe7c6c60d
Carmen <s.carmen2101@gmail.com>
2025-12-06 18:05:05 +0100

    add new selectors

---
+++ cypress/fixtures/routes.ts
@@ -0,0 +1,16 @@
+export const Routes = {
+  NEW_ROUTE_BTN: "[data-testid='empty-state-action']",
+  ROUTE_NAME: "[data-testid='route-form-name']",
+  SERVICE_LIST: "[data-testid='route-form-service-id']",
+  SERVICE_LIST_ITEM: "div[class='route-form-service-dropdown-item']",
+  TAGS_INPUT: "[data-testid='route-form-tags']",
+  BASIC_ROUTE_CONFIGURATION_RADIO_BTN:
+    "[data-testid='route-form-config-type-basic']",
+  PATHS_INPUT: "[data-testid='route-form-paths-input-1']",
+  STRIP_PATH_CHECKBOX: "[data-testid='route-form-strip-path']",
+  HOST_INPUT: "[data-testid='route-form-hosts-input-1']",
+  SUBMIT_BTN: "[data-testid='route-create-form-submit']",
+  METHODS_INPUT: "[data-testid='route-form-methods']",
+  METHODS_INPUT_GET: "[data-testid='multiselect-item-GET']",
+  ROUTE_ID: "[data-testid='id-property-value']",
+};
